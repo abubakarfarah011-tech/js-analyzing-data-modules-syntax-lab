7284df84e6024950d63b2c2f4195e0440bf7ab76
abubakar <abubakarfarah011@gmail.com>
2025-12-14 13:11:48 +0300

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+  
+  //importing datejs
+  var require=("datejs");
 
+
+function combineUsers(...args) {
+  //the return objects
+  const combinedObject = {
+    users:[]
+  }
+
+  //looping all arrays and merging 
+  args.forEach(array => {
+    combinedObject.users.push(...array)
+  })
+
+  //Add today's date in format M/d/yyyy
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy')
+
+  //Return the final object
+  return combinedObject
+}
 
 
 module.exports = {
